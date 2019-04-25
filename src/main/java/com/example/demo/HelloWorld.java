94dc6f6b9b169ffa49555be1388ad9117f152c74
chjp <1398704556@qq.com>
2019-04-25 20:35:28 +0800

    add输出666

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -10,5 +10,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("你好世界");
         System.out.println(66);
+        System.out.println("dev");
     }
 }
