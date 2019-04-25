8c1ddfaeccd2bd0676465d3076ed27519caf4af6
chjp <1398704556@qq.com>
2019-04-25 20:00:32 +0800

    修复 你好世界bug

---
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+/**
+ * Date:2019/4/25
+ * Author: JunPing be yourself
+ * Desc:
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hhh");
+    }
+}
